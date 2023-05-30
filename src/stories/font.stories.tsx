85378bb84eff510dde4acdb8d8e8984eb444b916
kuku <kuku.li@ikegps.com>
2023-05-31 01:12:43 +1200

    feat: add the initial font

---
+++ src/stories/font.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/fonts.scss';
+
+export default {
+    title: 'Typography/Mbtingle',
+};
+
+export const Default = () => (
+    <div style={{ fontFamily: 'Mbtingle', fontSize: '1.5rem', fontWeight: 800 }}>
+        MBTI nah gonna lie
+    </div>
+);
